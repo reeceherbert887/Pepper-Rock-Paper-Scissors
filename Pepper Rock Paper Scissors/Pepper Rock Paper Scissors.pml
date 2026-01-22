bbb5992ff4790e2e7a6c11992caaa3b69b944c10
reeceherbert887 <98314521+reeceherbert887@users.noreply.github.com>
2026-01-22 18:23:07 +0000

    Choregraphe Diagram 1.0

---
+++ Pepper Rock Paper Scissors/Pepper Rock Paper Scissors.pml
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="Pepper Rock Paper Scissors" format_version="4">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+        <BehaviorDescription name="behavior" src="Pepper Rock Paper Scissors/behavior_1" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs />
+    <Resources>
+        <File name="Pepper Rock Paper Scissors" src="Pepper Rock Paper Scissors/Pepper Rock Paper Scissors.pml" />
+        <File name="manifest" src="Pepper Rock Paper Scissors/manifest.xml" />
+    </Resources>
+    <Topics />
+    <IgnoredPaths />
+    <Translations auto-fill="en_US">
+        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+    </Translations>
+</Package>
---
+++ Pepper Rock Paper Scissors/translations/translation_en_US.ts
@@ -0,0 +1,82 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Hello humans, i am pepper</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello humans, i am pepper</translation>
+        </message>
+        <message>
+            <source>Certanly </source>
+            <comment>Text</comment>
+            <translation type="obsolete">Certanly </translation>
+        </message>
+        <message>
+            <source>Hello, Would you like to play a game?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello, Would you like to play a game?</translation>
+        </message>
+        <message>
+            <source>Rock, Paper, Scissors, Shoot!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Rock, Paper, Scissors, Shoot!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (1)</name>
+        <message>
+            <source>Im sorry i didnt catch that.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Im sorry i didnt catch that.</translation>
+        </message>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Amazing! let us begin</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Amazing! let us begin</translation>
+        </message>
+        <message>
+            <source>Rock, Paper, Scissors, Shoot!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Rock, Paper, Scissors, Shoot!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (2)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Hello, Would you like to play a game?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello, Would you like to play a game?</translation>
+        </message>
+        <message>
+            <location filename="../../../../../../../Program Files (x86)/Softbank Robotics/Choregraphe Suite 2.5/translations/behavior_1/behavior.xar" line="0"/>
+            <source></source>
+            <comment>Text</comment>
+            <translation></translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (3)</name>
+        <message>
+            <source>Rock, Paper, Scissors, Shoot!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Rock, Paper, Scissors, Shoot!</translation>
+        </message>
+    </context>
+</TS>
